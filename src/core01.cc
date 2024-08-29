3db3e1d07b44831e26c4bf994242e9098fca0841
sakibalam <sakiba12@nycstudents.net>
2024-08-29 19:21:58 -0400

    08/29

--- src/core01.cc
+++ src/core01.cc
@@ -1,87 +1,127 @@
+#pragma once
+
+#include <adf.h>
+using namespace adf;
+
 #include "core01.h"
+#include "core02.h"
+#include "core03.h"
+#include "core04.h"
+#include "core05.h"
+#include "core06.h"
 #include "core01lut.h"
+#include "core02lut.h"
+#include "core03lut.h"
+#include "core04lut.h"
+#include "core05lut.h"
+#include "core06lut.h"
 
-void kan_spline_kernel_core1(
-    const int num_knots,
-    input_window<float> *in,
-    input_window<float> *target,
-    output_window<float> *out,
-    output_window<float> *gradients,
-    float learning_rate,
-    float regularization_strength
-) {
-    const int vector_size = 8;
-    int i;
+class myGraph : public graph {
+private:
+    // Kernel declarations
+    kernel core01;
+    kernel core02;
+    kernel core03;
+    kernel core04;
+    kernel core05;
+    kernel core06;
 
-    for (i = 0; i + vector_size <= num_knots; i += vector_size) {
-        aie::vector<float, vector_size> input_vector = window_readincr_v<vector_size>(in);
-        aie::vector<float, vector_size> result_vector = aie::zeros<float, vector_size>();
-        aie::vector<float, vector_size> error_vector;
-        aie::vector<float, vector_size> grad_vector;
+    // Declare LUT parameters
+    parameter core01lut;
+    parameter core02lut;
+    parameter core03lut;
+    parameter core04lut;
+    parameter core05lut;
+    parameter core06lut;
 
-        for (int j = 0; j < vector_size; ++j) {
-            if (i + j < num_knots) {
-                aie::vector<float, vector_size> coeff_vector = aie::broadcast<float, vector_size>(spline_coefficients_1[i + j]);
-                aie::vector<float, vector_size> knot_vector = aie::broadcast<float, vector_size>(spline_knots_1[i + j]);
-                result_vector += coeff_vector * (input_vector - knot_vector);
-            }
-        }
+public:
+    input_plio in[4];
+    output_plio out[3];
 
-        // Pruning and sparsification step
-        float pruning_threshold = 0.1;  // Example threshold value for pruning
-        result_vector = select(result_vector > pruning_threshold, result_vector, 0.0f);
+    myGraph() {
+        // Define input/output PLIOs
+        in[0] = input_plio::create("prod_in1", plio_64_bits, "data/input01.txt");
+        in[1] = input_plio::create("prod_in2", plio_64_bits, "data/input02.txt");
+        in[2] = input_plio::create("prod_in3", plio_64_bits, "data/input03.txt");
+        in[3] = input_plio::create("prod_in4", plio_64_bits, "data/input04.txt");
 
-        window_writeincr(out, result_vector);
+        out[0] = output_plio::create("prod_out1", plio_64_bits, "data/output01.txt");
+        out[1] = output_plio::create("prod_out2", plio_64_bits, "data/output02.txt");
+        out[2] = output_plio::create("prod_out3", plio_64_bits, "data/output03.txt");
 
-        error_vector = result_vector - window_readincr_v<vector_size>(target);
-        grad_vector = 2.0f * (error_vector + regularization_strength * result_vector);
+        // Create kernels
+        core01 = kernel::create(core01_top);
+        core02 = kernel::create(core02_top);
+        core03 = kernel::create(core03_top);
+        core04 = kernel::create(core04_top);
+        core05 = kernel::create(core05_top);
+        core06 = kernel::create(core06_top);
 
-        for (int j = 0; j < vector_size; ++j) {
-            if (i + j < num_knots) {
-                spline_coefficients_1[i + j] -= learning_rate * grad_vector[j];
-            }
-        }
+        // Source files for the kernels
+        source(core01) = "core01.cc";
+        source(core02) = "core02.cc";
+        source(core03) = "core03.cc";
+        source(core04) = "core04.cc";
+        source(core05) = "core05.cc";
+        source(core06) = "core06.cc";
 
-        window_writeincr(gradients, grad_vector);
+        // Set kernel locations
+        location<kernel>(core01) = tile(8, 0);
+        location<kernel>(core02) = tile(8, 1);
+        location<kernel>(core03) = tile(8, 2);
+        location<kernel>(core04) = tile(8, 3);
+        location<kernel>(core05) = tile(8, 4);
+        location<kernel>(core06) = tile(8, 5);
+
+        // Define LUT parameters and connect them to kernels
+        core01lut = parameter::array(spline_coefficients_1);
+        core02lut = parameter::array(spline_coefficients_2);
+        core03lut = parameter::array(spline_coefficients_3);
+        core04lut = parameter::array(spline_coefficients_4);
+        core05lut = parameter::array(spline_coefficients_5);
+        core06lut = parameter::array(spline_coefficients_6);
+
+        connect<>(core01lut, core01);
+        connect<>(core02lut, core02);
+        connect<>(core03lut, core03);
+        connect<>(core04lut, core04);
+        connect<>(core05lut, core05);
+        connect<>(core06lut, core06);
+
+        // Connect kernels to input/output PLIOs
+        connect<>(in[0].out[0], core01.in[0]);
+        connect<>(core01.out[0], core02.in[0]);
+        connect<>(core02.out[0], out[0].in[0]);
+
+        connect<>(in[1].out[0], core03.in[0]);
+        connect<>(core03.out[0], core04.in[0]);
+        connect<>(core04.out[0], out[1].in[0]);
+
+        connect<>(in[2].out[0], core05.in[0]);
+        connect<>(core05.out[0], core06.in[0]);
+        connect<>(core06.out[0], out[2].in[0]);
+
+        // Single buffer management
+        single_buffer(core01.in[0]);
+        single_buffer(core02.in[0]);
+        single_buffer(core03.in[0]);
+        single_buffer(core04.in[0]);
+        single_buffer(core05.in[0]);
+        single_buffer(core06.in[0]);
+
+        single_buffer(core01.out[0]);
+        single_buffer(core02.out[0]);
+        single_buffer(core03.out[0]);
+        single_buffer(core04.out[0]);
+        single_buffer(core05.out[0]);
+        single_buffer(core06.out[0]);
+
+        // Runtime ratios for each kernel
+        runtime<ratio>(core01) = 0.6;
+        runtime<ratio>(core02) = 0.6;
+        runtime<ratio>(core03) = 0.6;
+        runtime<ratio>(core04) = 0.6;
+        runtime<ratio>(core05) = 0.6;
+        runtime<ratio>(core06) = 0.6;
     }
-
-    if (i < num_knots) {
-        int remaining_elements = num_knots - i;
-        aie::vector<float, 8> input_vector = window_readincr_v<8>(in);
-        aie::vector<float, 8> result_vector = aie::zeros<float, 8>();
-        aie::vector<float, 8> error_vector;
-        aie::vector<float, 8> grad_vector;
-
-        for (int j = 0; j < remaining_elements; ++j) {
-            aie::vector<float, 8> coeff_vector = aie::broadcast<float, 8>(spline_coefficients_1[i + j]);
-            aie::vector<float, 8> knot_vector = aie::broadcast<float, 8>(spline_knots_1[i + j]);
-            result_vector[j] += coeff_vector[j] * (input_vector[j] - knot_vector[j]);
-        }
-
-        // Pruning and sparsification step for remaining elements
-        result_vector = select(result_vector > pruning_threshold, result_vector, 0.0f);
-
-        window_writeincr(out, result_vector);
-
-        error_vector = result_vector - window_readincr_v<8>(target);
-        grad_vector = 2.0f * (error_vector + regularization_strength * result_vector);
-
-        for (int j = 0; j < remaining_elements; ++j) {
-            spline_coefficients_1[i + j] -= learning_rate * grad_vector[j];
-        }
-
-        window_writeincr(gradients, grad_vector);
-    }
-}
-
-void core01_top(
-    input_window<float> &__restrict inA,
-    input_window<float> &__restrict target,
-    output_window<float> &__restrict out,
-    output_window<float> &__restrict gradients,
-    float learning_rate,
-    float regularization_strength
-) {
-    const int num_knots = sizeof(spline_knots_1) / sizeof(spline_knots_1[0]);
-    kan_spline_kernel_core1(num_knots, &inA, &target, &out, &gradients, learning_rate, regularization_strength);
-}
+};
